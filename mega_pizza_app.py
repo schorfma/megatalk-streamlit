57bb68c04fe4fb7bc7260fdc14fd55362639b040
Martin Schorfmann <schorfma@uni-bremen.de>
2020-12-17 11:45:48 +0100

    Finished MegaPizza App

---
+++ mega_pizza_app.py
@@ -0,0 +1,141 @@
+from time import sleep
+from typing import List
+
+import streamlit
+
+from pizza_lib import (
+    PizzaCrust, PizzaSauce, PizzaTopping, Pizza, PizzaOrder,
+    PIZZA_SIZES,
+    PIZZA_CRUSTS, PIZZA_SAUCES, PIZZA_TOPPINGS
+)
+
+streamlit.set_page_config(
+    page_title="MegaPizza",
+    page_icon="pizza",
+    layout="centered"
+)
+
+streamlit.title("MegaPizza")
+
+streamlit.header("Number of Pizza Configurations")
+
+NUMBER_PIZZA_CONFIGURATIONS = streamlit.number_input(
+    "Select Number of Pizza Configurations",
+    min_value=0,
+    max_value=10,
+    step=1,
+    value=1
+)
+
+PIZZAS: List[Pizza] = []
+
+if NUMBER_PIZZA_CONFIGURATIONS == 0:
+    streamlit.info("We can't make zero Pizzas!")
+else:
+    streamlit.header("Configure your MegaPizza")
+
+for pizza_id in range(
+        1,
+        NUMBER_PIZZA_CONFIGURATIONS + 1
+):
+    streamlit.subheader(f"Pizza #{pizza_id}")
+
+    pizza_size = streamlit.select_slider(
+        "Select a Pizza Size",
+        PIZZA_SIZES,
+        format_func=lambda size: f"⌀{size}cm",
+        key=f"pizza_size#{pizza_id}"
+    )
+
+    pizza_crust = streamlit.radio(
+        "Select a Crust Type",
+        [
+            crust
+            for crust in PIZZA_CRUSTS
+            if crust.diameter == pizza_size
+        ],
+        key=f"pizza_crust#{pizza_id}"
+    )
+
+    streamlit.image(pizza_crust.image)
+
+    pizza_sauce = streamlit.radio(
+        "Select a Sauce",
+        [None] + [
+            sauce
+            for sauce in PIZZA_SAUCES
+            if sauce.diameter == pizza_size
+        ],
+        key=f"pizza_sauce#{pizza_id}"
+    )
+
+    if pizza_sauce:
+        streamlit.image(pizza_sauce.image)
+
+    pizza_toppings = streamlit.multiselect(
+        "Select one or multiple Toppings",
+        [
+            topping
+            for topping in PIZZA_TOPPINGS
+            if topping.diameter == pizza_size
+        ],
+        key=f"pizza_toppings#{pizza_id}"
+    )
+
+    for pizza_topping in pizza_toppings:
+        streamlit.image(pizza_topping.image)
+
+    pizza_name = streamlit.text_input(
+        "Pizza Name",
+        key=f"pizza_name#{pizza_id}"
+    )
+
+    pizza_number = streamlit.number_input(
+        "How many?",
+        min_value=1,
+        max_value=12,
+        value=1,
+        step=1,
+        key=f"pizza_number#{pizza_id}"
+    )
+
+    for index in range(pizza_number):
+        pizza = Pizza(
+            pizza_name,
+            pizza_crust,
+            pizza_sauce,
+            *pizza_toppings
+        )
+
+        streamlit.write(pizza)
+
+        PIZZAS.append(
+            pizza
+        )
+
+streamlit.header("Make Pizza Order")
+
+customer_name = streamlit.text_input("Customer Name")
+
+customer_phone_number = streamlit.text_input("Customer Phone Number")
+
+PIZZA_ORDER = PizzaOrder(
+    customer_name=customer_name,
+    customer_phone_number=customer_phone_number,
+    pizzas=PIZZAS
+)
+
+if streamlit.button(f"Authorize Payment of {PIZZA_ORDER.price:.2f}€"):
+
+    for pizza in PIZZA_ORDER:
+        streamlit.subheader(pizza.name)
+        pizza_progress = streamlit.progress(0)
+
+        with streamlit.spinner("Baking Pizza"):
+            for progress, waiting_time in enumerate(pizza.bake()):
+                sleep(waiting_time)
+                pizza_progress.progress(progress)
+
+        streamlit.image(pizza.image, caption=pizza.name)
+
+        streamlit.balloons()
--- pizza_lib.py
+++ pizza_lib.py
@@ -1,8 +1,9 @@
 from collections import UserList
 from math import pi, ceil
 from pathlib import Path
-from typing import List, Text, Union
+from typing import Iterable, List, Text, Union
 
+import numpy
 from PIL import Image
 
 class PizzaPart:
@@ -31,23 +32,27 @@ class PizzaPart:
     def price(self) -> float:
         return self.base_price * self.area
 
+    @property
+    def image(self) -> Text:
+        return self.image_path.as_posix()
+
     def __str__(self) -> Text:
-        return f"{self.name} (⌀{self.diameter}cm, {self.price:.2d}€)"
+        return f"{self.name} (⌀{self.diameter}cm, {self.price:.2f}€)"
 
 class PizzaCrust(PizzaPart):
-    base_duration: int
+    base_duration: float
 
     def __init__(
             self,
             name: Text,
-            price: float,
+            base_price: float,
             diameter: int,
             image_path: Union[Text, Path],
-            base_duration: int
+            base_duration: float
     ):
         super().__init__(
             name,
-            price,
+            base_price,
             diameter,
             image_path
         )
@@ -86,12 +91,25 @@ class Pizza:
         self.toppings = list(toppings)
 
     @property
+    def price(self) -> float:
+        pizza_price = self.crust.price
+
+        if self.sauce:
+            pizza_price += self.sauce.price
+
+        for topping in self.toppings:
+            pizza_price += topping.price
+
+        return float(f"{pizza_price:.2f}")
+
+    @property
     def image(self) -> Image:
-        crust = Image.open(self.crust.image_path)
-        sauce = Image.open(self.sauce.image_path)
+        crust = Image.open(self.crust.image_path) if self.crust else None
+        sauce = Image.open(self.sauce.image_path) if self.sauce else None
         toppings = [
             Image.open(topping.image_path)
             for topping in self.toppings
+            if topping
         ]
 
         stack = [crust, sauce, *toppings]
@@ -101,11 +119,23 @@ class Pizza:
             if part
         ]
 
+        pizza: Image
+
         pizza, *remaining = stack
 
         for layer in remaining:
-            pizza = pizza.paste(layer)
+            pizza.alpha_composite(layer)
 
+        return crust
+
+    def bake(self) -> List[float]:
+        return [
+            (self.crust.duration / 100)
+            for index in range(0, 100 + 1)
+        ]
+
+    def __str__(self) -> Text:
+        return f"{self.name} (⌀{self.crust.diameter}cm, {self.price}€)"
 
 class PizzaOrder(UserList):
     customer_name: Text
@@ -121,3 +151,202 @@ class PizzaOrder(UserList):
         self.customer_name = customer_name
         self.customer_phone_number = customer_phone_number
 
+    @property
+    def price(self) -> float:
+        order_price = 0.0
+
+        for pizza in self.data:
+            order_price += pizza.price
+
+        return float(f"{order_price:.2f}")
+
+SMALL_DIAMETER = 20
+
+MEDIUM_DIAMETER = 26
+
+FAMILY_DIAMETER = 40
+
+PIZZA_SIZES = [
+    SMALL_DIAMETER,
+    MEDIUM_DIAMETER,
+    FAMILY_DIAMETER
+]
+
+LIGHT_CRUST_PRICE = 0.025
+
+DARK_CRUST_PRICE = 0.035
+
+LIGHT_CRUST_DURATION = 0.05
+
+DARK_CRUST_DURATION = 0.075
+
+TOMATO_SAUCE_PRICE = 0.0005
+
+JOGHURT_SAUCE_PRICE = 0.0007
+
+TOMATO_TOPPING_PRICE = 0.0020
+
+PINEAPPLE_TOPPING_PRICE = 0.0025
+
+CHEESE_TOPPING_PRICE = 0.010
+
+MOZZARELLA_TOPPING_PRICE = 0.025
+
+PIZZA_CRUSTS = [
+    PizzaCrust(
+        name="Small Crust",
+        base_price=LIGHT_CRUST_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/crusts/crust-20cm.png",
+        base_duration=LIGHT_CRUST_DURATION
+    ),
+    PizzaCrust(
+        name="Medium Crust",
+        base_price=LIGHT_CRUST_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/crusts/crust-26cm.png",
+        base_duration=LIGHT_CRUST_DURATION
+    ),
+    PizzaCrust(
+        name="Family Crust",
+        base_price=LIGHT_CRUST_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/crusts/crust-40cm.png",
+        base_duration=LIGHT_CRUST_DURATION
+    ),
+    PizzaCrust(
+        name="Small Dark Crust",
+        base_price=DARK_CRUST_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/crusts/dark-crust-20cm.png",
+        base_duration=DARK_CRUST_DURATION
+    ),
+    PizzaCrust(
+        name="Medium Dark Crust",
+        base_price=DARK_CRUST_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/crusts/dark-crust-26cm.png",
+        base_duration=DARK_CRUST_DURATION
+    ),
+    PizzaCrust(
+        name="Family Dark Crust",
+        base_price=DARK_CRUST_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/crusts/dark-crust-40cm.png",
+        base_duration=DARK_CRUST_DURATION
+    )
+]
+
+PIZZA_SAUCES = [
+    PizzaSauce(
+        name="Tomato Sauce (Small)",
+        base_price=TOMATO_SAUCE_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/sauces/tomato-20cm.png"
+    ),
+    PizzaSauce(
+        name="Tomato Sauce (Medium)",
+        base_price=TOMATO_SAUCE_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/sauces/tomato-26cm.png"
+    ),
+    PizzaSauce(
+        name="Tomato Sauce (Small)",
+        base_price=TOMATO_SAUCE_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/sauces/tomato-40cm.png"
+    ),
+    PizzaSauce(
+        name="Joghurt Sauce (Small)",
+        base_price=JOGHURT_SAUCE_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/sauces/joghurt-20cm.png"
+    ),
+    PizzaSauce(
+        name="Joghurt Sauce (Medium)",
+        base_price=JOGHURT_SAUCE_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/sauces/joghurt-26cm.png"
+    ),
+    PizzaSauce(
+        name="Joghurt Sauce (Small)",
+        base_price=JOGHURT_SAUCE_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/sauces/joghurt-40cm.png"
+    )
+]
+
+PIZZA_TOPPINGS = [
+    PizzaTopping(
+        name="Tomato Slices (Small)",
+        base_price=TOMATO_TOPPING_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/toppings/tomato-20cm.png"
+    ),
+    PizzaTopping(
+        name="Tomato Slices (Medium)",
+        base_price=TOMATO_TOPPING_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/toppings/tomato-26cm.png"
+    ),
+    PizzaTopping(
+        name="Tomato Slices (Family)",
+        base_price=TOMATO_TOPPING_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/toppings/tomato-40cm.png"
+    ),
+    PizzaTopping(
+        name="Pineapple Pieces (Small)",
+        base_price=PINEAPPLE_TOPPING_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/toppings/pineapple-20cm.png"
+    ),
+    PizzaTopping(
+        name="Pineapple Pieces (Medium)",
+        base_price=PINEAPPLE_TOPPING_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/toppings/pineapple-26cm.png"
+    ),
+    PizzaTopping(
+        name="Pineapple Pieces (Family)",
+        base_price=PINEAPPLE_TOPPING_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/toppings/pineapple-40cm.png"
+    ),
+    PizzaTopping(
+        name="Cheese (Small)",
+        base_price=CHEESE_TOPPING_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/toppings/cheese-20cm.png"
+    ),
+    PizzaTopping(
+        name="Cheese (Medium)",
+        base_price=CHEESE_TOPPING_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/toppings/cheese-26cm.png"
+    ),
+    PizzaTopping(
+        name="Cheese (Family)",
+        base_price=CHEESE_TOPPING_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/toppings/cheese-40cm.png"
+    ),
+    PizzaTopping(
+        name="Mozzarella Cheese (Small)",
+        base_price=MOZZARELLA_TOPPING_PRICE,
+        diameter=SMALL_DIAMETER,
+        image_path="ingredients/toppings/mozzarella-20cm.png"
+    ),
+    PizzaTopping(
+        name="Mozzarella Cheese (Medium)",
+        base_price=MOZZARELLA_TOPPING_PRICE,
+        diameter=MEDIUM_DIAMETER,
+        image_path="ingredients/toppings/mozzarella-26cm.png"
+    ),
+    PizzaTopping(
+        name="Mozzarella Cheese (Family)",
+        base_price=MOZZARELLA_TOPPING_PRICE,
+        diameter=FAMILY_DIAMETER,
+        image_path="ingredients/toppings/mozzarella-40cm.png"
+    )
+]
